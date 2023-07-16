a6bf8c9a1042888449137e119486e2c839e37f5d
LizaM <liza.makarichak2019@gmail.com>
2023-07-16 19:36:32 +0300

    goit-js-hw-10

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
+axios.defaults.headers.common["x-api-key"] = "live_ifYEKjAPHTIh5Jo0el1oTPVTiBdDHxjjlUD6ithjbmMrCDu08ALmrMtNGMSBMhwi";
+import {fetchBreeds, fetchCatByBreed} from './cat-api';
+const selectBreeds = document.querySelector('.breed-select');
+const wrapperForCats = document.querySelector('.cat-info');
+const failureText = document.querySelector('.error');
+const loaderImg = document.querySelector('.loader');
+fetchBreeds().then(data => {
+    setTimeout(() => {
+        selectBreeds.style.display = 'flex';
+        const makeOptionInSelect = ({ id, name }) => `<option value = '${id}'> ${name} </option>`;
+        const markUp = data.map(obj => makeOptionInSelect(obj));
+        loaderImg.style.display = 'none';
+        selectBreeds.insertAdjacentHTML('beforeend', markUp);
+        new SlimSelect({
+            select: '.breed-select',
+        })
+    }, 1000)}).catch(err => {
+        Notiflix.Notify.failure(failureText);
+});
+selectBreeds.addEventListener("change", onSelectCat);
+function onSelectCat(e) {
+    e.preventDefault();
+    wrapperForCats.innerHTML = "";
+    const selectQueryId = e.target.value;
+    loaderImg.style.display = 'flex';
+    fetchCatByBreed(selectQueryId).
+        then(catData => {
+            loaderImg.style.display = 'none';
+            createCatMarkUp(catData);
+        }).
+        catch(err => { Notiflix.Notify.failure(failureText) });
+};
